9bbf6de0839e8afe46cf4dd38bbe83e271b2092e
Olexandr Tkachuk <temishena@gmail.com>
2023-03-18 19:48:30 +0200

    asda

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
-export const App = () => {
+import React from 'react'
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      
     </div>
-  );
-};
+  )
+}
