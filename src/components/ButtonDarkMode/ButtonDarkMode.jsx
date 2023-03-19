81ab010ff6aad1000ee375015963e2198160415c
Olexandr Tkachuk <temishena@gmail.com>
2023-03-19 16:57:22 +0200

    haha

--- src/components/ButtonDarkMode/ButtonDarkMode.jsx
+++ src/components/ButtonDarkMode/ButtonDarkMode.jsx
@@ -1,15 +1,28 @@
-// import { useState } from 'react';  
+import { useEffect, useState, useRef } from 'react';
 import sun from '../../img/icons/sun.svg';
 import moon from '../../img/icons/moon.svg';
 import './style.css';
 
 const ButtonDarkMode = () => {
-  // const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+  const btnRef = useRef(null);
 
-  const toggleDarkMode = () => {};
+  useEffect(() => {
+    if (darkMode) {
+      window.document.body.classList.add('dark');
+      btnRef.current.classList.add('dark-mode-btn--active');
+    } else {
+      window.document.body.classList.remove('dark');
+      btnRef.current.classList.remove('dark-mode-btn--active');
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
-    <button className="dark-mode-btn" onClick={toggleDarkMode}>
+    <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
       <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
       <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
     </button>
